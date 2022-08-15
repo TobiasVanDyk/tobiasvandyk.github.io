0afaf5e221d136242902cff98cb2b2009629ecbb
Tobias van Dyk <TobiasVanDyk@outlook.com>
2022-08-15 21:14:58 +0200

    Update hw.java

--- hw/hw.java
+++ hw/hw.java
@@ -1,7 +1,5 @@
-/*package whatever //do not write package name here */
- 
+/*package */
 import java.io.*;
- 
 class GFG {
     public static void main (String[] args) {
        System.out.println("Hello World");
